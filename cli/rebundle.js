991a3349ff3dc1801383e266fd987e1014f6019c
Jan Bölsche <regular.gonzales@googlemail.com>
2015-04-25 14:33:21 +0200

    fixed re-bundle subcommand

--- cli/rebundle.js
+++ cli/rebundle.js
@@ -2,6 +2,7 @@ var shellby = require('shellby');
 var async = require('async');
 var spawn = require('child_process').spawn;
 var path = require('path');
+var chalk = require('chalk');
 
 module.exports = function(config) {
     var list = require('./toc')(config).listRepos;
@@ -10,20 +11,23 @@ module.exports = function(config) {
         var wd = path.join(process.env.HOME, '.bpm/clones');
         var ewd = path.join(wd, episode.name);
 
-        console.log(episode.name);
         var commands = [
             'mkdir -p ' + wd,
             'rm -rf '+ ewd +' || exit 0',
-            'git clone ' + episode.ssh_url + ' ' + ewd,
-            'cd ' + ewd + '; npm install'
+            ['git clone ' + episode.ssh_url + ' ' + ewd, {
+                stdio: null
+            }],
+            ['git checkout master', {cwd: ewd}],
+            ['npm install', {cwd: ewd}],
+            ['bpm publish', {cwd: ewd}]
         ];
         shellby.series(commands, function(err) {
-            if (err) return cb(err);
-            var bpm = spawn('bpm', ['publish'], { cwd: ewd, stdio: 'inherit' });
-            bpm.on('close', function(code) {
-                if (code !== 0) return cb(new Error('creating remote repository failed.'));
-                cb(null);
-            });
+            if (err) {
+                console.error('FAILED to re-bundle ' + episode.name);
+            } else {
+                console.log(chalk.green('\u2713'), chalk.blue(episode.name));
+            }
+            cb(err);
         });
     }
 
@@ -32,7 +36,8 @@ module.exports = function(config) {
     api.rebundleAll = function(cb) {
         list(function(err, episodes) {
             if (err) return cb(err);
-            async.map(episodes, rebundleEpisode, cb);              
+            console.log(chalk.blue('about to republish', episodes.length, 'episodes.'), chalk.red('Please stand by ...'));
+            async.map(episodes, rebundleEpisode, cb);
         });
     };
 
