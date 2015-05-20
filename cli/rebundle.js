fa78d184f888303edd6fa226b14e51a655f049cb
Jan Bölsche <regular.gonzales@googlemail.com>
2015-05-20 18:39:42 +0200

    using terminal-status-board to show rebundle progress, fixes #16

--- cli/rebundle.js
+++ cli/rebundle.js
@@ -1,34 +1,67 @@
-var shellby = require('shellby');
-var async = require('async');
-var spawn = require('child_process').spawn;
 var path = require('path');
 var chalk = require('chalk');
+var Board = require('terminal-status-board');
+var pipeline = require('progress-pipeline');
+var _ = require('lodash');
+
+var spawn = require('child_process').spawn;
+var util = require('util');
+
+function shell(cmd, opts, cb) {
+    if (typeof opts ===  'function') {
+        cb = opts;
+        opts = { stdio: 'inherit' };
+    }
+    var parts = cmd.split(/\s+/g);
+    var p = spawn( parts[0], parts.slice(1), opts );
+
+    p.on('error', function (err) {
+        if (cb) cb(err);
+    });
+
+    p.on('exit', function(code){
+        var err = null;
+        if (code) {
+            err = new Error(util.format('command %s exited with status code %s', cmd, code));
+            err.code = code;
+            err.cmd = cmd;
+        }
+        if (cb) cb(err);
+    });
+}
+
+function shellJob(cmd, opts) {
+    var args = Array.prototype.slice.apply(arguments);
+    var job =  function(cb) {
+        args.push(cb);
+        shell.apply(null, args);
+    };
+    job.title = (opts ? opts.jobTitle : null) || cmd;
+    return job;
+}
 
 module.exports = function(config) {
     var list = require('./toc')(config).listRepos;
     
-    function rebundleEpisode(episode, cb) {
+    function makePipeline(episode) {
         var wd = path.join(process.env.HOME, '.bpm/clones');
         var ewd = path.join(wd, episode.name);
 
-        var commands = [
-            'mkdir -p ' + wd,
-            'rm -rf '+ ewd +' || exit 0',
-            ['git clone ' + episode.ssh_url + ' ' + ewd, {
-                stdio: null
-            }],
-            ['git checkout master', {cwd: ewd}],
-            ['npm install', {cwd: ewd}],
-            ['bpm publish', {cwd: ewd}]
+        var jobs = [
+            shellJob('mkdir -p ' + wd),
+            shellJob('rm -rf '+ ewd +' || exit 0'),
+            shellJob(
+                'git clone ' + episode.ssh_url + ' ' + ewd,
+                {
+                    jobTitle: 'cloning',
+                    stdio: null
+                }
+            ),
+            shellJob('git checkout master', {cwd: ewd}),
+            shellJob('npm install', {cwd: ewd}),
+            shellJob('bpm publish', {cwd: ewd})
         ];
-        shellby.series(commands, function(err) {
-            if (err) {
-                console.error('FAILED to re-bundle ' + episode.name);
-            } else {
-                console.log(chalk.green('\u2713'), chalk.blue(episode.name));
-            }
-            cb(err);
-        });
+        return pipeline(jobs);
     }
 
     var api = {};
@@ -37,7 +70,21 @@ module.exports = function(config) {
         list(function(err, episodes) {
             if (err) return cb(err);
             console.log(chalk.blue('about to republish', episodes.length, 'episodes.'), chalk.red('Please stand by ...'));
-            async.map(episodes, rebundleEpisode, cb);
+            
+            var board = Board();
+            var errors = [];
+
+            _.map(episodes, function(e) {
+                var p = makePipeline(e);
+                p.on('error', function(err) {
+                    errors.push(err);
+                });
+                board.add(p, e.name);
+            });
+            board.on('end', function() {
+                console.log(util.format('done (%d errors)', errors.length));
+                cb(errors.length ? errors : null);
+            }).pipe(process.stdout);
         });
     };
 
